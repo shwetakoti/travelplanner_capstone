9d928897ba684681dc699c003cddde8fc5ddc9c9
“Shweta <choti@ravikirans-air.lan>
2018-04-14 20:44:57 -0700

    commiting all files

--- test/test-server.js
+++ test/test-server.js
@@ -11,7 +11,7 @@ describe('sample test',function(){
   it('should return a status of 200',function(){
     return chai.request(app).get('/').then(function(res){
       expect(res).to.have.status(201);
-      expect(res).to.be.json;
+      expect(res).to.be.html;
     })
   })
 })
