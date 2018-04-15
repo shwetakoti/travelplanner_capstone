5b6eaf7d2e4dda651441c88f7c67700e2759564c
“Shweta <choti@ravikirans-air.lan>
2018-04-14 22:10:41 -0700

    modified server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + 'index.html');
 });
 
-app.listen(process.env.PORT||8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
 
 module.exports = {app} ;
