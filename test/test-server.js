8a24107f3bbc30662f1ab0c0aff0747795f5a400
“Shweta <choti@ravikirans-air.lan>
2018-04-14 21:56:06 -0700

    modified test-server.js

--- test/test-server.js
+++ test/test-server.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 
 const {app} = require('../server');
 
-describe('sample test',function(){
+describe('server',function(){
   it('should return a status of 200',function(){
     return chai.request(app).get('/').then(function(res){
       expect(res).to.have.status(200);
