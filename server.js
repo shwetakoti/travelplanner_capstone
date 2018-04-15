f3a8e7aa8f49e62f1d556daa04d4ba9468a56f8b
“Shweta <choti@ravikirans-air.lan>
2018-04-14 20:54:51 -0700

    added get to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html');
+});
+
 app.listen(process.env.PORT||8080);
-
-
-
